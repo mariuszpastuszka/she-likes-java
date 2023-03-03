0e339e2e18ccf107963c5b58246d344665ce5e99
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-03-03 20:06:47 +0100

    int data type

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // 100 it's number literal (just value)
+        System.out.println(10000000);
+
+        // variable:
+        // holder for data
+        // needs good name e.g. myDaughterAge
+        // needs type of variable
+        // that data type limit possible set of allowed values
+        // int - integer numbers
+        int myDaughterAge = 5;
+
+        // assigning new value to variable
+        myDaughterAge = 6;
+        // sout + enter
+        System.out.println(myDaughterAge);
     }
 }
