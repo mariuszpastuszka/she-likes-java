3edc3e910f841940110312b0bbda2bd0ff6bab24
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-03-03 20:28:35 +0100

    introducing other data types: double, boolean, String

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,27 @@ public class Main {
         myDaughterAge = 6;
         // sout + enter
         System.out.println(myDaughterAge);
+
+        // use camel case starting with lower letter
+        // String type is for words and letters
+        String myName = "Mariusz";
+        String mySurname = "Pastuszka";
+        System.out.println(myName + "-" + mySurname);
+
+        String five = "5";
+        String otherFive = "5";
+        System.out.println(five + otherFive);
+
+        // true and false
+        boolean trueValue = true;
+        trueValue = false;
+
+        System.out.println(trueValue);
+
+        // 11.0
+        int eleven = 11;
+
+        // double type for number with fraction part
+        double trueEleven = 11.0;
     }
 }
