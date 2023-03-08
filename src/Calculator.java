d60ac25c5ea461fae1fdd5dc4c6c3ed358654833
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-03-08 18:03:12 +0100

    Calculator - homework

--- src/Calculator.java
+++ src/Calculator.java
@@ -18,5 +18,38 @@ public class Calculator {
         System.out.println("You provided number: " + secondNumber);
         int sum = firstNumber + secondNumber;
         System.out.println("Sum is: " + sum);
+
+        System.out.println("--------------------");
+        System.out.println("Calculating difference of two numbers");
+        System.out.println("Provide first number: ");
+        firstNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + firstNumber);
+        System.out.println("Provide second number: ");
+        secondNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + secondNumber);
+        int difference = firstNumber - secondNumber;
+        System.out.println("Difference is: " + difference);
+
+        System.out.println("--------------------");
+        System.out.println("Calculating product of two numbers");
+        System.out.println("Provide first number: ");
+        firstNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + firstNumber);
+        System.out.println("Provide second number: ");
+        secondNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + secondNumber);
+        int productOfNumbers = firstNumber * secondNumber;
+        System.out.println("Product of two numbers is: " + productOfNumbers);
+
+        System.out.println("--------------------");
+        System.out.println("Calculating quotient of two numbers");
+        System.out.println("Provide first number: ");
+        firstNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + firstNumber);
+        System.out.println("Provide second number: ");
+        secondNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + secondNumber);
+        int quotient = firstNumber / secondNumber;
+        System.out.println("Quotient is: " + quotient);
     }
 }
