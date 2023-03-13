a32cdd20f1a6831c9ff8ccdc1c81b6b86bbbd9db
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-03-13 19:54:30 +0100

    do-while refactored to while loop

---
+++ src/com/sda/she_likes_java/loops/do_while/FromDoWhileToWhileRefactoring.java
@@ -0,0 +1,21 @@
+package com.sda.she_likes_java.loops.do_while;
+
+public class FromDoWhileToWhileRefactoring {
+
+    public static void main(String[] args) {
+        int numberOfTrials = 3;
+        do {
+            System.out.println("Current value of number of trials: " + numberOfTrials);
+            numberOfTrials--;
+        } while (numberOfTrials > 0);
+
+        System.out.println("Let's refactor that to while loop");
+        numberOfTrials = 3;
+
+        while (numberOfTrials > 0) {
+            System.out.println("While loop, number of trials: " + numberOfTrials);
+            numberOfTrials--;
+        }
+
+    }
+}
