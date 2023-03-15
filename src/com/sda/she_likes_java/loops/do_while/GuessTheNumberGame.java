6f23f0e46bc36b7ade0f3c75f4bb0fa8ba25933c
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-03-15 19:56:57 +0100

    guess the number game - with do/while loop

---
+++ src/com/sda/she_likes_java/loops/do_while/GuessTheNumberGame.java
@@ -0,0 +1,23 @@
+package com.sda.she_likes_java.loops.do_while;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumberGame {
+
+    public static void main(String[] args) {
+        Random numberGenerator = new Random();
+        int someRandomNumber = numberGenerator.nextInt(10);
+//        System.out.println("random number provided by java is: " + someRandomNumber);
+
+        Scanner inputReader = new Scanner(System.in);
+        int userAnswer;
+        System.out.println("Play game with me:)");
+        do {
+            System.out.println("Guess a number between 1 and 10");
+            userAnswer = inputReader.nextInt();
+        } while (userAnswer != someRandomNumber);
+
+        System.out.println("Great, You win!!!");
+    }
+}
