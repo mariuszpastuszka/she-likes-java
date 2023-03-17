b4ab31a29751355b613d4045a2d9bdafa66de4ba
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-03-17 20:37:22 +0100

    java time formatter

--- src/com/sda/she_likes_java/time_and_date/TimeAndDateExample.java
+++ src/com/sda/she_likes_java/time_and_date/TimeAndDateExample.java
@@ -2,6 +2,7 @@ package com.sda.she_likes_java.time_and_date;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class TimeAndDateExample {
 
@@ -15,5 +16,9 @@ public class TimeAndDateExample {
 
         LocalDateTime currentDateAndTime = LocalDateTime.now();
         System.out.println("Current date and time in my location is: " + currentDateAndTime);
+
+        System.out.println("Let's format a little bit our date and time");
+//        System.out.println(currentDateAndTime.format(DateTimeFormatter.RFC_1123_DATE_TIME));
+        System.out.println(currentDateAndTime.format(DateTimeFormatter.BASIC_ISO_DATE));
     }
 }
