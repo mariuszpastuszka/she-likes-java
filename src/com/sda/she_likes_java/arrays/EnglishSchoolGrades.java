06188d17256065128baa8d5352771c4121d1cfcf
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-03-20 20:48:35 +0100

    grades example

---
+++ src/com/sda/she_likes_java/arrays/EnglishSchoolGrades.java
@@ -0,0 +1,21 @@
+package com.sda.she_likes_java.arrays;
+
+import java.util.Random;
+
+public class EnglishSchoolGrades {
+
+    public static void main(String[] args) {
+        int[] myGradesOnEnglishClass = new int[10000];
+
+        Random gradeGenerator = new Random();
+
+        // intitializing grades with some random scores
+        for (int index = 0; index < myGradesOnEnglishClass.length; index++) {
+            myGradesOnEnglishClass[index] = 1 + gradeGenerator.nextInt(6);
+        }
+
+        for (int index = 0; index < myGradesOnEnglishClass.length; index++) {
+            System.out.println("my grade on index nr: " + index + " is: " + myGradesOnEnglishClass[index]);
+        }
+    }
+}
