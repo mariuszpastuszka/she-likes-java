d331c1f7558a3e664e728f455de63a138c86d4b4
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-04-03 10:34:19 +0200

    comments with memory stages added

--- src/com/sda/she_likes_java/objects/HouseExample.java
+++ src/com/sda/she_likes_java/objects/HouseExample.java
@@ -6,24 +6,30 @@ public class HouseExample {
         House myHouse = new House(4, 2);
         myHouse.setNumberOfFloors(2);
         System.out.println("I've got new house: " + myHouse);
+        // stage 1
 
         System.out.println("Currently my house has got " + myHouse.getNumberOfRooms() + " rooms");
         System.out.println("I need more rooms...");
         myHouse.setNumberOfRooms(5);
+        // stage 2
 
         System.out.println("Hey, I've got more rooms now...");
         System.out.println("Currently my house has got " + myHouse.getNumberOfRooms() + " rooms");
 
         System.out.println("=======================================");
         HouseOwner wife = new HouseOwner("Mary", myHouse);
+        // stage 3
         HouseOwner husband = new HouseOwner("Jack", myHouse);
+        // stage 4
 
         System.out.println("Mary would like to repair her house");
         wife.getHouse().setNumberOfBathrooms(3); // House house = wife.getHouse(); house.setNumberOfBathrooms(3);
         System.out.println("Currently the house looks like this: " + myHouse);
+        // stage 5
 
         husband.getHouse().setNumberOfBathrooms(1);
         System.out.println("Currently the house looks like this: " + myHouse);
+        // stage 6
 
         System.out.println("Mary: " + wife);
         System.out.println("Jack: " + husband);
@@ -33,11 +39,14 @@ public class HouseExample {
         System.out.println("Now myHouse reference: " + myHouse);
         System.out.println("Mary: " + wife);
         System.out.println("Jack: " + husband);
+        // stage 7
 
         myHouse = new House(10, 10);
         wife.setHouse(myHouse);
+        // stage 8
         System.out.println("Mary: " + wife);
         System.out.println("Jack: " + husband);
         wife.setHouse(husband.getHouse());
+        // stage 9
     }
 }
