519bae1a3795aefd5a3e81475aa07e57c9bc1049
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-05-08 17:00:06 +0200

    solution for exercise 22

---
+++ src/com/sda/she_likes_java/homework/exercise_22/ShoppingCart.java
@@ -0,0 +1,30 @@
+package com.sda.she_likes_java.homework.exercise_22;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+
+    private List<Item> items;
+
+    public ShoppingCart() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addProductToBasket(Item product) {
+        items.add(product);
+    }
+
+    public void displayProducts() {
+        System.out.println("Items in shopping cart: " + items);
+    }
+
+    public double calculateTotalBasketValue() {
+        double sum = 0;
+        for (int i = 0; i < items.size(); i++) {
+            sum += items.get(i).getPriceInUSD();
+        }
+
+        return sum;
+    }
+}
---
+++ src/com/sda/she_likes_java/homework/exercise_22/package-info.java
@@ -0,0 +1,15 @@
+package com.sda.she_likes_java.homework.exercise_22;
+
+/**
+ * Create a shopping cart class.
+ * Add as a field an items variable of type List<Item> - to store products.
+ * Item is also the class to be created.
+ * It is to contain the following fields:
+ * - product name
+ * - product price.
+ * The shopping cart class should provide the following operations:
+ * - add product to basket
+ * - displaying products in the shopping cart
+ * - calculation of the total basket value.
+ * Create a Main class and check the operations of your classes.
+ */
