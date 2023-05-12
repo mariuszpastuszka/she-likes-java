eb879046e899c11a4d6f2e73745660410264fa84
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-05-12 16:17:20 +0200

    homework 24 solution

---
+++ src/com/sda/she_likes_java/homework/exercise_24/EnglishPolishDictionary.java
@@ -0,0 +1,31 @@
+package com.sda.she_likes_java.homework.exercise_24;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EnglishPolishDictionary {
+
+    private final Map<String, String> dictionary;
+
+    public EnglishPolishDictionary() {
+        dictionary = new HashMap<>();
+        dictionary.put("house", "dom");
+        dictionary.put("sun", "słońce");
+        dictionary.put("moon", "księżyc");
+        dictionary.put("sky", "niebo");
+        dictionary.put("tree", "drzewo");
+        dictionary.put("school", "szkoła");
+        dictionary.put("pupil", "uczeń");
+        dictionary.put("teacher", "nauczyciel");
+        dictionary.put("blackboard", "tablica");
+    }
+
+    public void translate(String englishWord) {
+        String polishTranslation = dictionary.get(englishWord);
+        if (polishTranslation != null) {
+            System.out.printf("English word: [%s] means [%s] in Polish%n", englishWord, polishTranslation);
+        } else {
+            System.out.printf("Don't know how to translate [%s] into Polish%n", englishWord);
+        }
+    }
+}
---
+++ src/com/sda/she_likes_java/homework/exercise_24/Main.java
@@ -0,0 +1,13 @@
+package com.sda.she_likes_java.homework.exercise_24;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Let's learn some Polish");
+
+        EnglishPolishDictionary englishPolishDictionary = new EnglishPolishDictionary();
+        englishPolishDictionary.translate("sky");
+        englishPolishDictionary.translate("school");
+        englishPolishDictionary.translate("dog");
+    }
+}
