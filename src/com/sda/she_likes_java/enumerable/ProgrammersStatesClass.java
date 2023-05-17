751550f5f42d7e0abef4bb0a908aef3cbc5a625c
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-05-17 20:39:17 +0200

    enums, enums:)

--- src/com/sda/she_likes_java/enumerable/ProgrammersStatesClass.java
+++ src/com/sda/she_likes_java/enumerable/ProgrammersStatesClass.java
@@ -8,6 +8,15 @@ public class ProgrammersStatesClass {
     public static final ProgrammersStatesClass NORMAL =
             new ProgrammersStatesClass("yeah it's just life");
 
+    private static ProgrammersStatesClass[] value() {
+
+        ProgrammersStatesClass[] values = new ProgrammersStatesClass[3];
+        values[0] = GOD_MODE;
+        values[1] = EXHAUSTED;
+        values[2] = NORMAL;
+        return values;
+    }
+
     String description;
 
     public String getDescription() {
