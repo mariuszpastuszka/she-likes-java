292381215456280dc3991228d4fb18e8a71cb311
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-05-19 18:15:58 +0200

    time and dates examples

--- src/com/sda/she_likes_java/time_and_date/TimeAndDateExample.java
+++ src/com/sda/she_likes_java/time_and_date/TimeAndDateExample.java
@@ -3,6 +3,9 @@ package com.sda.she_likes_java.time_and_date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.Date;
 
 public class TimeAndDateExample {
 
@@ -20,5 +23,17 @@ public class TimeAndDateExample {
         System.out.println("Let's format a little bit our date and time");
 //        System.out.println(currentDateAndTime.format(DateTimeFormatter.RFC_1123_DATE_TIME));
         System.out.println(currentDateAndTime.format(DateTimeFormatter.BASIC_ISO_DATE));
+
+        // DON'T USE!!!!
+        Date oldDate = new Date();
+        oldDate.setTime(1000 * 60 * 60 * 24);
+
+        LocalDate now = LocalDate.now();
+        LocalDate future = now.plus(5, ChronoUnit.DAYS);
+        System.out.println("now is: " + now);
+        System.out.println("future is: " + future);
+        LocalDate previousYear = now.minusYears(1);
+        System.out.println("previous year: " + previousYear);
+        boolean isBefore = previousYear.isBefore(now);
     }
 }
