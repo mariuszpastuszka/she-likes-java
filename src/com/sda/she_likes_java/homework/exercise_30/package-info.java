9e643ce1ca1c47b098b0ea248dbf75774eed8f60
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-05-29 17:27:55 +0200

    homework 30 - solution

---
+++ src/com/sda/she_likes_java/homework/exercise_30/package-info.java
@@ -0,0 +1,14 @@
+package com.sda.she_likes_java.homework.exercise_30;
+
+/*
+Exercise 30
+Write SQL scripts:
+- Create a Student table with fields:
+- ID of type Integer
+- Name type varchar(255)
+- Surname type varchar(255)
+- Age type Integer
+- SEX type varchar(255)
+Write a script to insert 5 rows into the Student table
+Add a script to delete students by ID
+ */
