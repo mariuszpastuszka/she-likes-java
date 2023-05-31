86500226cceca3026b8bd064cc72f1795c3339b9
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-05-31 20:00:57 +0200

    Crud repository for AccentureStudent

---
+++ src/com/sda/she_likes_java/database_in_memory/AccentureStudentsRepository.java
@@ -0,0 +1,31 @@
+package com.sda.she_likes_java.database_in_memory;
+
+import java.util.List;
+
+// CRUD
+// Create
+// Read
+// Update
+// Delete
+public class AccentureStudentsRepository {
+
+    public List<AccentureStudent> getAllStudents() {
+        return List.of();
+    }
+
+    public AccentureStudent getStudentById(Long id) {
+        return null;
+    }
+
+    public AccentureStudent saveStudent(AccentureStudent student) {
+        return null;
+    }
+
+    public boolean deleteStudentById(Long id) {
+        return true;
+    }
+
+    public AccentureStudent updateStudent(AccentureStudent student) {
+        return null;
+    }
+}
