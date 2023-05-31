b6f52dd8a107d30673669b61907cdd3f0082959d
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-05-31 19:37:59 +0200

    h2 in memory - exmaple started

---
+++ src/com/sda/she_likes_java/database_in_memory/DbCredentials.java
@@ -0,0 +1,10 @@
+package com.sda.she_likes_java.database_in_memory;
+
+public class DbCredentials {
+
+    public static final String h2InMemoryUrl = "jdbc:h2:mem:test-db";
+
+    public static final String h2Username = "sa";
+
+    public static final String h2Password = "sa";
+}
