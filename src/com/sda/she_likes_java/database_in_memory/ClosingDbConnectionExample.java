2a13441d826ac9638b7ec756c356c3ab4da943ad
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-06-02 18:54:06 +0200

    Try with resources example

--- src/com/sda/she_likes_java/database_in_memory/ClosingDbConnectionExample.java
+++ src/com/sda/she_likes_java/database_in_memory/ClosingDbConnectionExample.java
@@ -27,7 +27,14 @@ public class ClosingDbConnectionExample {
             }
         }
 
-        //
+        System.out.println("Now let's use better way to close connection - available since Java 7");
+        System.out.println("Try with resources construction");
+        try (Connection conn = DbConnectionUtils.getConnectionRiskyWay(DbCredentials.h2InMemoryUrl,
+                DbCredentials.h2Username, DbCredentials.h2Password)) {
+
+        } catch (SQLException e) {
+            System.out.println("Some exception: " + e);
+        }
     }
 
     private static void handleString(String string) {
