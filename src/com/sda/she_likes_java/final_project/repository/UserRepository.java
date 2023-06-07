db5e0b8fc72c6cea510dde19a83a63fa8ab5735a
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-06-07 18:57:46 +0200

    Initial version of UserRepository

---
+++ src/com/sda/she_likes_java/final_project/repository/UserRepository.java
@@ -0,0 +1,33 @@
+package com.sda.she_likes_java.final_project.repository;
+
+import com.sda.she_likes_java.final_project.entity.User;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class UserRepository {
+
+    public void registerNewUser() {
+
+    }
+
+    public void deleteUser() {
+
+    }
+
+    public List<User> listAllUsers() {
+        return List.of();
+    }
+
+    public void updateUser(User user) {
+
+    }
+
+    public List<User> getUsersBasedOnCriteria() {
+        return List.of();
+    }
+
+    public List<User> getOrderedUsers(Comparator<User> comparator) {
+        return List.of();
+    }
+}
--- src/com/sda/she_likes_java/final_project/todo.md
+++ src/com/sda/she_likes_java/final_project/todo.md
@@ -3,7 +3,7 @@
 - See available cars
 - See rent cars
 - Register new user
-  - limiting access if user is not experience driver 
+  - limiting access if user is not experienced driver 
 - Renting car feature
   - filtering based on user input
 - Calendar with all events
