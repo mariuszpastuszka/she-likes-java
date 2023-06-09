2429ad2bc9c8ae986a12a0da89fe8c20bb667441
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-06-09 19:33:04 +0200

    Final commit

--- src/com/sda/she_likes_java/final_project/service/BookingService.java
+++ src/com/sda/she_likes_java/final_project/service/BookingService.java
@@ -1,6 +1,11 @@
 package com.sda.she_likes_java.final_project.service;
 
+import com.sda.she_likes_java.final_project.configuration.CarRecordConfig;
+import com.sda.she_likes_java.final_project.entity.*;
 import com.sda.she_likes_java.final_project.repository.BookingRecordRepository;
+
+import java.time.Duration;
+import java.time.LocalDate;
 
 public class BookingService {
 
@@ -11,7 +16,29 @@ public class BookingService {
         this.bookingRecordRepository = bookingRecordRepository;
     }
 
-    public void bookCar() {
+    public BookingRecord bookCar(User user,
+                        Car carToBook,
+                        LocalDate startDate,
+                        LocalDate endDate,
+                        boolean isPaid,
+                        PayingMethod payingMethod) {
         System.out.println("New car booking");
+
+        // calculate the price for booking
+        long bookingFinalPrice = calculateWholePrice(carToBook, startDate, endDate);
+
+        // create booking record
+        BookingRecord bookingRecord = new BookingRecord(null, user, carToBook, startDate,
+                endDate, bookingFinalPrice, isPaid, payingMethod);
+
+        return bookingRecordRepository.storeBookingRecord(bookingRecord);
+    }
+
+    private static long calculateWholePrice(Car carToBook, LocalDate startDate, LocalDate endDate) {
+        PriceCategory priceCategory = carToBook.category();
+        long pricePerDayInEuroCents = CarRecordConfig.getPricePerDayInEuroCents(priceCategory);
+        Duration datesDuration = Duration.between(startDate, endDate);
+        long numberOfDays = datesDuration.toDays();
+        return pricePerDayInEuroCents * numberOfDays;
     }
 }
